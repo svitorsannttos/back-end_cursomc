323a0329ac8d4f6a295e8aef8ae0054d2b46817d
Vitor Santos <vitor330y@gmail.com>
2022-03-13 12:53:10 -0300

    endpoint delete categoria

--- src/main/java/com/vitor/cursomc/services/CategoriaService.java
+++ src/main/java/com/vitor/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.vitor.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.vitor.cursomc.domain.Categoria;
 import com.vitor.cursomc.repositories.CategoriaRepository;
+import com.vitor.cursomc.services.exceptions.DataIntegrityException;
 import com.vitor.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -24,5 +26,21 @@ public class CategoriaService {
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
+	}
+	
+	public Categoria update (Categoria obj){
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete (Integer id){
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos!");
+		}
+		
 	}
 }
