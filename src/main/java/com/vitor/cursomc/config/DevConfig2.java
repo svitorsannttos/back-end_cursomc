a2b9afbd7d9dbb570468aec3921d2742adef31f4
svitorsannttos <vitor330y@gmail.com>
2022-04-14 20:14:55 -0300

    criando profile dev e etst

--- src/main/java/com/vitor/cursomc/config/DevConfig2.java
+++ src/main/java/com/vitor/cursomc/config/DevConfig2.java
@@ -3,6 +3,7 @@ package com.vitor.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,14 +11,22 @@ import org.springframework.context.annotation.Profile;
 import com.vitor.cursomc.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig2 {
 
 	@Autowired
 	private DBService databasetest;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	@Bean
-	public boolean instantiateDatabase() throws ParseException {
+	public Boolean instantiateDatabase() throws ParseException {
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
 		databasetest.InstantiateTestDatabase();
 		return true;
 	}
